df9d9fad728772547092ab56df39ba1b3bf1f864
Tzachi Gratziani <tzachi.gratziani@tufin.com>
2017-01-27 18:49:43 +0200

    Refactoring

--- src/main/java/com/tzachi/st/ViolationHelper.java
+++ src/main/java/com/tzachi/st/ViolationHelper.java
@@ -12,6 +12,11 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class ViolationHelper {
+    private static final String USP_URL = "https://{0}/securetrack/api/violations/access_requests/sync.json?use_topology=false&ar_domain_mode=false";
+    private static final String TAG_URL = "https://{0}/securetrack/api/tagpolicy/violation_check?policy_external_id=";
+    private static final String APPLICATION_XML = "application/xml";
+    private static final String APPLICATION_JSON = "application/json";
+
     private Logger logger;
 
     public ViolationHelper() {
@@ -24,21 +29,19 @@ public class ViolationHelper {
     }
 
     public SecurityPolicyViolationsForMultiArDTO checkUSPAccessRequestViolation(HttpHelper stHelper, String str) throws IOException{
-        final String uspURL = "https://{0}/securetrack/api/violations/access_requests/sync.json?use_topology=false&ar_domain_mode=false";
         System.out.println("Checking USP access request violation");
         JSONObject response = new JSONObject();
         SecurityPolicyViolationsForMultiArDTO violationMultiAr = null;
-        response = stHelper.post(uspURL, str, "application/xml");
+        response = stHelper.post(USP_URL, str, APPLICATION_XML);
         violationMultiAr = new SecurityPolicyViolationsForMultiArDTO(response);
         return violationMultiAr;
     }
 
     public void checkTagViolation(HttpHelper stHelper, String body, String policyId) throws IOException {
-        final String tagURL = "https://{0}/securetrack/api/tagpolicy/violation_check?policy_external_id=" + policyId;
-        logger.info("Tag Violation");
+        System.out.println("Tag Violation");
         System.out.println(body);
         JSONObject response = new JSONObject();
-        response = stHelper.post(tagURL, body, "application/json");
+        response = stHelper.post(TAG_URL + policyId, body, APPLICATION_JSON);
         System.out.println(response);
     }
 }
